5e67d8f46bb4a44eb0ae1b0bca7cb09d61868652
Leticia Oliveira <le.andrades@gmail.com>
2023-12-06 16:02:23 +0000

    Portfolio

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+var name = 'Leticia Oliveira';
+console.log(name)
+
+let age = 28;
+console.log(age)
+
+let nationality = 'Brazilian';
+console.log(nationality)
+
+console.log(name, age, nationality)
+console.log(' Hello, my name is ' + name + ' and I am '+ age + ' and I am ' + nationality + '!')
+
+var education = 'Quality Control Technology';
+console.log(education)
+
+let language = 'English';
+console.log(language)
+
+let skills = 'Agile Methodologies';
+console.log(skills)
+
+console.log(education, language, skills)
+console.log(' I completed ' + education + ' course and I speak '+ language + ' and one of my skills is ' + skills + ' . ')
